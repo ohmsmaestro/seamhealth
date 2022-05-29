b22222f5fc13bf46f6c5fa3b2c78c9dfa8857012
OhmsDev <muhammed.omenesa@microbridge.com.ng>
2022-05-29 17:41:31 +0100

    Project setup

---
+++ src/main/java/com/api/seamhealth/SeamhealthApplication.java
@@ -0,0 +1,13 @@
+package com.api.seamhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeamhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeamhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/seamhealth/SeamhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.seamhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeamhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
